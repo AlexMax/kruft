00c7dfb5ba7cd930da639a36c12fb5c55eaa6299
Lexi Mayfield <me@leximayfield.com>
2024-12-21 23:07:44 -0500

    Ensure our test runner actually fails

--- tests/zztest.h
+++ tests/zztest.h
@@ -33,6 +33,7 @@
 #endif
 
 #include <limits.h>
+#include <stddef.h>
 
 // Boolean datatype.
 typedef int ZZT_BOOL;
@@ -857,7 +858,7 @@ int zzt_run_all(void)
         }
     }
 
-    return 0;
+    return failed != 0;
 }
 
 #if defined(ZZTEST_SELFCHECK)
